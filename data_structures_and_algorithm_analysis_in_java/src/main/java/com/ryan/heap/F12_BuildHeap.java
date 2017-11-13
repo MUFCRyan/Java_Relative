07d22fdfecbdeb9d12d4d9edecfd48c3fc93f94c
赵逢春 <ryanformufc@gmail.com>
2017-11-13 22:15:12 +0800

    写练习 6.12

---
+++ data_structures_and_algorithm_analysis_in_java/src/main/java/com/ryan/heap/F12_BuildHeap.java
@@ -0,0 +1,18 @@
+package com.ryan.heap;
+
+import com.ryan.tree.Node;
+
+/**
+ * Created by zhaofengchun on 2017/11/13.
+ * 练习 6.12 编写一个程序输入 N 个元素并
+ *      a. 将它们逐个插入堆中
+ *      b. 以线性时间建立一个堆
+ *      比较这两个算法对于已排序、反序以及随机输入的运行时间
+ */
+
+public class F12_BuildHeap<AnyType extends Comparable<? super AnyType>> {
+
+    private Node<AnyType> buildHeapVerbatim(Node<AnyType> node, int[] nums){
+        return null;
+    }
+}
