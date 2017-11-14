f20df5e1a9c72a947245d6654a31a91ee65419a7
赵逢春 <ryanformufc@gmail.com>
2017-11-14 22:17:21 +0800

    完成练习 6.12，待测试

--- data_structures_and_algorithm_analysis_in_java/src/main/java/com/ryan/heap/F12_BuildHeap.java
+++ data_structures_and_algorithm_analysis_in_java/src/main/java/com/ryan/heap/F12_BuildHeap.java
@@ -1,7 +1,5 @@
 package com.ryan.heap;
 
-import com.ryan.tree.Node;
-
 /**
  * Created by zhaofengchun on 2017/11/13.
  * 练习 6.12 编写一个程序输入 N 个元素并
@@ -10,9 +8,60 @@ import com.ryan.tree.Node;
  *      比较这两个算法对于已排序、反序以及随机输入的运行时间
  */
 
-public class F12_BuildHeap<AnyType extends Comparable<? super AnyType>> {
+public class F12_BuildHeap {
+    private static final int MAX_SIZE = 1000;
 
-    private Node<AnyType> buildHeapVerbatim(Node<AnyType> node, int[] nums){
-        return null;
+    private void buildHeapVerbatim(Integer[] elements){
+        Integer[] heap = new Integer[MAX_SIZE];
+        for (Integer element : elements) {
+            insertVerbatim(heap, element);
+        }
+    }
+
+    private boolean insertVerbatim(Integer[] heap, Integer element){
+        // 1. 找到第一个空位
+        if (heap == null || heap.length == 0)
+            return false;
+        int position = 1;
+        while (heap[position] != null)
+            position ++;
+
+        // 2. 和空位的父节点的值进行比较，如果 >= 父节点的值或者父节点不存在就停止，否则与父节点交换继续进行比较，父节点所在的位置 = 子节点位置 / 2
+        while (position > 0){
+            int compareResult = element.compareTo(heap[position / 2]);
+            if (compareResult >= 0){
+                heap[position] = heap[position / 2]; // 父节点下移
+                position /= 2; // 子节点上滤
+            } else {
+                heap[position] = element;
+                break;
+            }
+        }
+        return true;
+    }
+
+    private void buildHeapOnce(Integer[] elements){
+        Integer[] heap = new Integer[MAX_SIZE];
+        int heapSize = 0;
+        for (int i = 0; i < elements.length; i++) {
+            heap[i + 1] = elements[i];
+            heapSize ++;
+        }
+        for (int size = heapSize / 2; size > 0; size--) {
+            // 左儿子位置 2i，右儿子位置 2i + 1
+            Integer min = heap[size * 2];
+            if (size * 2 + 1 <= heapSize - 1){
+                min = Math.min(heap[size * 2], heap[size * 2 + 1]);
+            }
+            int compareResult = min.compareTo(heap[size]);
+            if (compareResult < 0){
+                if (heap[size * 2].compareTo(heap[size * 2 + 1]) < 0){
+                    heap[size * 2] = heap[size];
+                } else {
+                    heap[size * 2 + 1] = heap[size];
+                }
+                heap[size] = min;
+            }
+        }
     }
 }
